45b94df8c207c23724f589953dced24279bb50a3
Vlad <vlasisla1988@gmail.com>
2023-08-08 17:08:08 +0300

    feat: added base settings and pages with styles

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { container, navLinks, navLinkItem, navLinkText, heading } from './layout.module.scss';
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout;
